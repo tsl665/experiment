079c15ad115448cd6d994ee96f4e0f59293ecc10
Sunli Tang <sunlitang@172-16-187-114.DYNAPOOL.NYU.EDU>
2016-03-03 15:09:34 -0500

    update plot_rate

--- bf_blkfft/plot_rate.m
+++ bf_blkfft/plot_rate.m
@@ -1,8 +1,25 @@
+
+%% Load and process data
 dataFileName = 'fftKron_blkSize_conv2.mat';
+dataFileName2 = 'fftKron_blkSize_conv2_plus2.mat';
 load(dataFileName);
+dara1 = data;
+load(dataFileName2);
+data2 = data;
+clear data
+for k = 1:length(data1.n)
+    data.n{k} = [dara1.n{k}, data2.n{k}];
+    data.nnz1{k} = [dara1.nnz1{k}, data2.nnz1{k}];
+    data.nnz2{k} = [dara1.nnz2{k}, data2.nnz2{k}];
+    data.rate{k} = [dara1.rate{k}, data2.rate{k}];
+    data.blkSize{k} = [dara1.blkSize{k}, data2.blkSize{k}];
+    data.nEmb{k} = [dara1.nEmb{k}, data2.nEmb{k}];
+end
+
+%% Plot
 figure
-hold on
 semilogx(data.n{1}, data.rate{1},'b');
+hold on
 semilogx(data.n{2}, data.rate{2},'r');
 semilogx(data.n{3}, data.rate{3},'k');
 semilogx(data.n{4}, data.rate{4},'g');
